94b9acb486195bedc3ac3ecbdc719ccee2096ace
Victoria Leon <vcrleon@gmail.com>
2018-01-06 12:10:37 -0500

    Variable class completed

--- src/com/company/Variables.java
+++ src/com/company/Variables.java
@@ -1,5 +1,28 @@
 package com.company;
 
 public class Variables {
+    public static void main(String[] args) {
+
+        int i = 113;
+        double d = 2.71828;
+        String s = "Computer Science";
+
+        System.out.println("This is room # " + i);
+        System.out.println("e is close to " + d);
+        System.out.println("I am learning a bit about " + s);
+
+    }
+
+    public static boolean isOdd(int number) {
+        return true;
+    }
+
+    public static String printHelloWorld() {
+        return "Hello World";
+    }
+
+    public static int greatestCommonFactor() {
+        return 0;
+    }
 
 }
